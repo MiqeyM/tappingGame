cc1b49702d75d4a56966edf6803c3ef49ff4ad0d
Mikolaj <mikolajjm@gmail.com>
2019-05-11 17:51:17 +0200

    testing other viewmodel

--- app/src/main/java/com/example/android/tappinggame/ScoreActivity.kt
+++ app/src/main/java/com/example/android/tappinggame/ScoreActivity.kt
@@ -6,22 +6,22 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.tappinggame.recycler.ScoreAdapter
-import com.example.android.tappinggame.viewmodel.ScoreViewModel
+
 
 class ScoreActivity : AppCompatActivity() {
 
 //    private val viewModel by lazy(::provideViewModel)
 
-
+private val scoreViewModel by lazy { ViewModelProviders.of(this)}
+private val adapter = ScoreAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.score_recycler_view)
-        val adapter = ScoreAdapter()
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.layoutManager = LinearLayoutManager(this)
 
 
 
--- app/src/main/java/com/example/android/tappinggame/viewmodel/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/tappinggame/viewmodel/ScoreViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.tappinggame.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.android.tappinggame.database.ScoreDao
 import com.example.android.tappinggame.database.ScoresRepository
 import com.example.android.tappinggame.database.ScoresRoomDatabase
 import com.example.android.tappinggame.entity.ScoreEntry
@@ -11,18 +13,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
-class ScoreViewModel(application: Application) : ViewModel() {
+//class ScoreViewModel(application: Application) : ViewModel() {
+//
+//    private val repository : ScoresRepository
+//    val allScores: LiveData<List<ScoreEntry>>
+//
+//    init{
+//        val scoreDao = ScoresRoomDatabase.getDatabase(application).scoreDao()
+//        repository = ScoresRepository(scoreDao)
+//        allScores = repository.allScores
+//    }
+//
+//    fun insert(score: ScoreEntry) =  viewModelScope.launch(Dispatchers.IO){
+//        repository.insert(score)
+//    }
+//}
 
-    private val repository : ScoresRepository
-    val allScores: LiveData<List<ScoreEntry>>
+class MoviesViewModel(application: Application): AndroidViewModel(application) {
+    private val scoreDao: ScoreDao = ScoresRoomDatabase.getDatabase(application).scoreDao()
 
-    init{
-        val scoreDao = ScoresRoomDatabase.getDatabase(application).scoreDao()
-        repository = ScoresRepository(scoreDao)
-        allScores = repository.allScores
-    }
+    fun getScores() = scoreDao.getScores()
 
-    fun insert(score: ScoreEntry) =  viewModelScope.launch(Dispatchers.IO){
-        repository.insert(score)
-    }
 }
